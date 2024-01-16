8dd7761aba13aeb3c9a752540b59e1f477d76f69
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2024-01-16 04:09:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# VacAIgent: Streamlit-Integrated AI Crew for Trip Planning
+
+(_Forked and enhanced from the crewAI examples repository_)
+
+## Introduction
+
+VacAIgent leverages the CrewAI framework to automate and enhance the trip planning experience, integrating a user-friendly Streamlit interface. This project demonstrates how autonomous AI agents can collaborate and execute complex tasks efficiently, now with an added layer of interactivity and accessibility through Streamlit.
+
+- Originally developed by [@joaomdmoura](https://x.com/joaomdmoura)
+
+## CrewAI Framework
+
+CrewAI simplifies the orchestration of role-playing AI agents. In VacAIgent, these agents collaboratively decide on cities and craft a complete itinerary for your trip based on specified preferences, all accessible via a streamlined Streamlit user interface.
+
+## Streamlit Interface
+
+The introduction of Streamlit transforms this application into an interactive web app, allowing users to easily input their preferences and receive tailored travel plans.
+
+## Running the Application
+
+To experience the VacAIgent app:
+
+- **Configure Environment**: Set up the environment variables for [Browseless](https://www.browserless.io/), [Serper](https://serper.dev/), and [OpenAI](https://openai.com/). Use the `.secrets.example` as a guide to add your keys then move that file (`secrets.toml`) to `.streamlit/secrets.toml`.
+
+- **Install Dependencies**: Execute `pip install -r requirements.txt` in your terminal.
+- **Launch the App**: Run `streamlit run streamlit_app.py` to start the Streamlit interface.
+
+★ **Disclaimer**: The application uses GPT-4 by default. Ensure you have access to OpenAI's API and be aware of the associated costs.
+
+## Details & Explanation
+
+- **Streamlit UI**: The Streamlit interface is implemented in `streamlit_app.py`, where users can input their trip details.
+- **Components**:
+  - `./trip_tasks.py`: Contains task prompts for the agents.
+  - `./trip_agents.py`: Manages the creation of agents.
+  - `./tools directory`: Houses tool classes used by agents.
+  - `./streamlit_app.py`: The heart of the Streamlit app.
+
+## Using GPT 3.5
+
+To switch from GPT-4 to GPT-3.5, pass the llm argument in the agent constructor:
+
+```python
+from langchain.chat_models import ChatOpenAI
+
+llm = ChatOpenAI(model='gpt-3.5') # Loading GPT-3.5
+
+class TripAgents:
+    # ... existing methods
+
+    def local_expert(self):
+        return Agent(
+            role='Local Expert',
+            goal='Provide insights about the selected city',
+            tools=[SearchTools.search_internet, BrowserTools.scrape_and_summarize_website],
+            llm=llm,
+            verbose=True
+        )
+
+```
+
+## Using Local Models with Ollama
+
+For enhanced privacy and customization, you can integrate local models like Ollama:
+
+### Setting Up Ollama
+
+- **Installation**: Follow Ollama's guide for installation.
+- **Configuration**: Customize the model as per your requirements.
+
+### Integrating Ollama with CrewAI
+
+Pass the Ollama model to agents in the CrewAI framework:
+
+```python
+from langchain.llms import Ollama
+
+ollama_model = Ollama(model="agent")
+
+class TripAgents:
+    # ... existing methods
+
+    def local_expert(self):
+        return Agent(
+            role='Local Expert',
+            tools=[SearchTools.search_internet, BrowserTools.scrape_and_summarize_website],
+            llm=ollama_model,
+            verbose=True
+        )
+
+```
+
+## Benefits of Local Models
+
+- **Privacy**: Process sensitive data in-house.
+- **Customization**: Tailor models to fit specific needs.
+- **Performance**: Potentially faster responses with on-premises models.
+
+## License
+
+VacAIgent is open-sourced under the MIT License.
---
+++ streamlit_app.py
@@ -0,0 +1,140 @@
+from crewai import Crew
+from textwrap import dedent
+from trip_agents import TripAgents
+from trip_tasks import TripTasks
+import streamlit as st
+import datetime
+
+st.set_page_config(page_icon="✈️", layout="wide")
+
+def icon(emoji: str):
+    """Shows an emoji as a Notion-style page icon."""
+    st.write(
+        f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
+        unsafe_allow_html=True,
+    )
+
+class TripCrew:
+
+  def __init__(self, origin, cities, date_range, interests):
+    self.cities = cities
+    self.origin = origin
+    self.interests = interests
+    self.date_range = date_range
+
+  def run(self, update_callback=None):
+    agents = TripAgents()
+    tasks = TripTasks()
+    
+    city_selector_agent = agents.city_selection_agent()
+    local_expert_agent = agents.local_expert()
+    travel_concierge_agent = agents.travel_concierge()
+  
+    if update_callback:
+            update_callback("Initializing trip planning process...")
+    identify_task = tasks.identify_task(
+      city_selector_agent,
+      self.origin,
+      self.cities,
+      self.interests,
+      self.date_range
+    )
+
+    if update_callback:
+            update_callback("Agent is gathering tasks...")
+    gather_task = tasks.gather_task(
+      local_expert_agent,
+      self.origin,
+      self.interests,
+      self.date_range
+    )
+
+    plan_task = tasks.plan_task(
+      travel_concierge_agent, 
+      self.origin,
+      self.interests,
+      self.date_range
+    )
+
+    crew = Crew(
+      agents=[
+        city_selector_agent, local_expert_agent, travel_concierge_agent
+      ],
+      tasks=[identify_task, gather_task, plan_task],
+      verbose=True
+    )
+
+    result = crew.kickoff()
+
+    if update_callback:
+            update_callback("Agent is finalizing tasks...")
+
+    return result
+
+if __name__ == "__main__":
+  icon("🏖️ VacAIgent")
+
+  st.info("**Let AI agents plan your next vacation...**")
+  
+  import datetime
+
+  today = datetime.datetime.now().date() 
+  next_year = today.year + 1
+  jan_16_next_year = datetime.date(next_year, 1, 10)
+
+  with st.sidebar:
+    st.header("👇 Enter your trip details")
+    with st.form("my_form"):
+      location = st.text_input("Where are you currently located?")
+      cities = st.text_input("City and country are you interested in vacationing at?")
+      date_range = st.date_input(
+        "Date range you are interested in traveling?",
+        min_value=today, 
+        value=(today, jan_16_next_year + datetime.timedelta(days=6)), 
+        format="MM/DD/YYYY",
+      )
+      interests = st.text_area("High level interests and hobbies or extra details about your trip?")
+
+      submitted = st.form_submit_button("Submit")
+    
+    st.divider()
+    
+    # Credits to joaomdmoura/CrewAI for the code: https://github.com/joaomdmoura/crewAI
+    st.sidebar.markdown(
+        """
+        Credits to [**@joaomdmoura**](https://twitter.com/joaomdmoura) 
+        for creating **crewAI** 🚀
+        """,
+        unsafe_allow_html=True
+    )
+    
+    st.sidebar.info("Click the logo to visit GitHub repo", icon="👇")
+    st.sidebar.markdown(
+        """
+        <a href="https://github.com/joaomdmoura/crewAI" target="_blank">
+            <img src="https://raw.githubusercontent.com/joaomdmoura/crewAI/main/docs/crewai_logo.png" alt="CrewAI Logo" style="width:100px;"/>
+        </a>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+def update_status(status_container, message):
+    """Callback function for updates"""
+    status_container.write(message)
+
+if submitted:
+    with st.status("**Grab a coffee or go for a ~10 minute walk, processing your trip plan...**", expanded=True) as status:
+      start_time = datetime.datetime.now() 
+
+      trip_crew = TripCrew(location, cities, date_range, interests)
+      result = trip_crew.run(update_callback=lambda msg: update_status(status, msg))
+
+      end_time = datetime.datetime.now()  
+      time_taken = end_time - start_time
+
+      # Update the status container to indicate completion and time taken
+      status.update(f"✅ Trip Plan Ready! Time taken: {time_taken}", state="complete", expanded=False)
+  
+    st.subheader("Here is your Trip Plan", anchor=False, divider="rainbow")
+    st.markdown(result)
